48b2513088490f82e49df512661f264d8da7db03
Daniel Gomes <danielgomes1@live.com>
2022-01-02 15:10:21 -0300

    trocando de sqlite to mysql

--- src/database/config/DatabaseConfig.ts
+++ src/database/config/DatabaseConfig.ts
@@ -1,11 +1,14 @@
+// eslint-disable-next-line @typescript-eslint/no-var-requires
+const env = require("dotenv-safe");
+
 const DatabaseConfig = {
   // dialect: Dialect = "mysql";
-  dialect: "sqlite",
+  dialect: "mysql",
   host: "localhost",
-  // username = "root";
-  // password = "root";
+  username: env.config().parsed?.DB_USER,
+  password: env.config().parsed?.DB_PASSWORD,
   database: "camille_api",
-  storage: "./database.sqlite",
+  // storage: "./database.sqlite",
 };
 
 module.exports = DatabaseConfig;
