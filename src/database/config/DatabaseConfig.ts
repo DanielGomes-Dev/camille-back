be08cd232f009e1d004f87a12bdd4668b1f8fda8
Daniel Gomes <danielgomes1@live.com>
2022-01-03 19:03:10 -0300

    fix: db migrate

--- src/database/config/DatabaseConfig.ts
+++ src/database/config/DatabaseConfig.ts
@@ -1,13 +1,14 @@
 // eslint-disable-next-line @typescript-eslint/no-var-requires
-const env = require("dotenv-safe");
+require("dotenv").config();
 
+console.log(process.env.DB_HOST, "ok");
 const DatabaseConfig = {
   // dialect: "sqlite",
-  dialect: env.config().parsed?.DB_DIALECT,
-  host: env.config().parsed?.DB_HOST,
-  username: env.config().parsed?.DB_USER,
-  password: env.config().parsed?.DB_PASSWORD,
-  database: env.config().parsed?.DB_NAME,
+  dialect: process.env.DB_DIALECT,
+  host: process.env.DB_HOST,
+  username: process.env.DB_USER,
+  password: process.env.DB_PASSWORD,
+  database: process.env.DB_NAME,
   // storage: "./database.sqlite",
 };
 
