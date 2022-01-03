678926ed88518b3aa1e71157fe94f54a98d4fe67
Daniel Gomes <danielgomes1@live.com>
2022-01-03 18:31:34 -0300

    fix: db

--- src/database/config/DatabaseConfig.ts
+++ src/database/config/DatabaseConfig.ts
@@ -2,13 +2,13 @@
 const env = require("dotenv-safe");
 
 const DatabaseConfig = {
-  dialect: "sqlite",
-  // dialect: env.config().parsed?.DB_DIALECT,
-  host: "localhost",
-  // username: env.config().parsed?.DB_USER,
-  // password: env.config().parsed?.DB_PASSWORD,
-  database: "camille_api",
-  storage: "./database.sqlite",
+  // dialect: "sqlite",
+  dialect: env.config().parsed?.DB_DIALECT,
+  host: env.config().parsed?.DB_HOST,
+  username: env.config().parsed?.DB_USER,
+  password: env.config().parsed?.DB_PASSWORD,
+  database: env.config().parsed?.DB_NAME,
+  // storage: "./database.sqlite",
 };
 
 module.exports = DatabaseConfig;
