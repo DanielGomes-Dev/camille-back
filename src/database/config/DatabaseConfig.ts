2b4a8674d540bbc66f6eb9990d98da835b3ddaa0
Daniel Gomes <danielgomes1@live.com>
2022-03-07 01:05:14 -0300

    fix: seeds history

--- src/database/config/DatabaseConfig.ts
+++ src/database/config/DatabaseConfig.ts
@@ -7,6 +7,7 @@ const DatabaseConfig = {
   username: process.env.DB_USER,
   password: process.env.DB_PASSWORD,
   database: process.env.DB_NAME,
+  seederStorage: "sequelize",
 };
 
 module.exports = DatabaseConfig;
